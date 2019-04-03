0755c6737a1932bc41b29edd721f7f21f02d6323
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-04-03 01:59:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-/* code goes here */
+
+let div = document.getElementById("hidden-div")
+
+document.addEvenetListener("DOMContentLoaded", replaceText(div))
+
+
+function replaceText(innerDiv){
+  innerDiv.innerHTML = "301"
+}
